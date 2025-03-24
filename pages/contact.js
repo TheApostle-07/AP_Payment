6a23f7cf089455861eddd110ab571608c0bb9ff6
Rufus Bright <rufusbright595@gmail.com>
2025-03-24 15:07:30 +0530

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -45,6 +45,9 @@ export default function Contact() {
               <a href="/">Home</a>
             </li>
             <li>
+              <a href="/aboutus">About Us</a>
+            </li>
+            <li>
               <a href="/privacy">Privacy Policy</a>
             </li>
             <li>
